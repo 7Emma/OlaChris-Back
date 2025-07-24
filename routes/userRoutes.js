16cdea53cdd078b6649d0abee848e75b454bc9a6
Emmanuel AGBOTOEDO <205164160+emmaagbo@users.noreply.github.com>
2025-07-24 03:28:56 +0100

    ola

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,19 +13,3 @@ router.get("/orders", authenticateToken, userController.getRecentOrders);
 router.get("/favorites", authenticateToken, userController.getFavoriteProducts);
 
 module.exports = router;
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-const { authenticateToken } = require("../middlewares/authMiddleware"); // Votre middleware JWT
-
-// Récupérer le profil de l'utilisateur actuel
-router.get("/profile", authenticateToken, userController.getUserProfile);
-// Mettre à jour le profil de l'utilisateur actuel
-router.put("/profile", authenticateToken, userController.updateUserProfile);
-
-// Routes pour les autres onglets (protégées par authentification)
-router.get("/orders", authenticateToken, userController.getRecentOrders);
-router.get("/favorites", authenticateToken, userController.getFavoriteProducts);
-
-
-module.exports = router;
